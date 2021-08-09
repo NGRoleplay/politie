083c0734d57776681d5b9e0a24c1cc3604d8eff1
TimRaben <88652714+NGRoleplay@users.noreply.github.com>
2021-08-09 11:27:02 +0200

    int
    init

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const botConfig = require("./botConfig.json");
 const Discord = require('discord.js');
+const fs = require("fs");
+
 const bot = new Discord.Client();
 
-bot.on('ready', () => {
-    console.log('Ready.')
+bot.on('ready', async () => {
+    console.log('Ready.');
 
 })
 
